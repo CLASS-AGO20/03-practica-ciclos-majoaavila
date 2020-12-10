6eaefb0a30207dc0d32d919bbce6ae92a0ba68ff
Majo Ávila Amezcua <mavila7@ucol.mx>
2020-12-10 16:03:53 -0600

    implemented factorial()

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
-export default class App {}
+export default class App {
+    factorial(numero) {
+        let multiplicacion = 1;
+
+        for(let i = 1; i <= numero; i = i + 1) {
+            multiplicacion = multiplicacion * i;
+        }
+        
+        return multiplicacion;
+    }
+}
 
 let app = new App();
+
+console.log("Probando factorial");
+console.log(app.factorial(5));
